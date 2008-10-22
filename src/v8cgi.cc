a82cc3f39a5fd6b80c80a801abe9a3c1caf58d89
ondrej.zara <none@none>
2008-10-22 12:52:04 +0000

    Global circular reference
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%4027

--- src/v8cgi.cc
+++ src/v8cgi.cc
@@ -201,6 +201,7 @@ int main(int argc, char ** argv, char ** envp) {
   context->Global()->Set(v8::String::New("include"), v8::FunctionTemplate::New(_include)->GetFunction());
   context->Global()->Set(v8::String::New("exit"), v8::FunctionTemplate::New(_exit)->GetFunction());
   context->Global()->Set(v8::String::New("onexit"), v8::FunctionTemplate::New(_onexit)->GetFunction());
+  context->Global()->Set(v8::String::New("global"), context->Global());
 
   SetupSys(envp, context->Global());
   SetupIo(context->Global());  

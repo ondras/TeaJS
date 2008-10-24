c1cd1af0363d298fa342adad1dbea99bb58bae5b
ondrej.zara <none@none>
2008-10-24 08:47:22 +0000

    removed the default shell behavior
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%4039

--- src/v8cgi.cc
+++ src/v8cgi.cc
@@ -11,52 +11,7 @@
 #include <cstdlib>
 #include <sstream>
 
-void RunShell(v8::Handle<v8::Context> context);
-bool ExecuteString(v8::Handle<v8::String> source,
-                   v8::Handle<v8::Value> name,
-                   bool print_result,
-                   bool report_exceptions);
-v8::Handle<v8::String> ReadFile(const char* name);
-void ReportException(v8::TryCatch* handler);
-
 v8::Handle<v8::Array> onexit;
-
-void die(int code) {
-    int max = onexit->Length();
-    v8::Handle<v8::Function> fun;
-    for (int i=0;i<max;i++) {
-	fun = v8::Handle<v8::Function>::Cast(onexit->Get(v8::Integer::New(i)));
-	fun->Call(v8::Context::GetCurrent()->Global(), 0, NULL);
-    }
-    exit(code);
-}
-
-v8::Handle<v8::Value> _include(const v8::Arguments& args) {
-  for (int i = 0; i < args.Length(); i++) {
-    v8::HandleScope handle_scope;
-    v8::String::Utf8Value file(args[i]);
-    v8::Handle<v8::String> source = ReadFile(*file);
-    if (source.IsEmpty()) {
-      return v8::ThrowException(v8::String::New("Error loading file"));
-    }
-    
-    if (!ExecuteString(source, v8::String::New(*file), false, true)) {
-      return v8::ThrowException(v8::String::New("Error executing file"));
-    }
-  }
-  return v8::Undefined();
-}
-
-
-v8::Handle<v8::Value> _exit(const v8::Arguments& args) {
-  die(args[0]->Int32Value());
-  return v8::Undefined();
-}
-
-v8::Handle<v8::Value> _onexit(const v8::Arguments& args) {
-  onexit->Set(v8::Integer::New(onexit->Length()), args[0]);
-  return v8::Undefined();
-}
 
 v8::Handle<v8::String> ReadFile(const char* name) {
   FILE* file = fopen(name, "rb");
@@ -85,55 +40,6 @@ v8::Handle<v8::String> ReadFile(const char* name) {
   delete[] chars;
   return result;
 }
-
-
-void RunShell(v8::Handle<v8::Context> context) {
-  static const int kBufferSize = 256;
-  while (true) {
-    char buffer[kBufferSize];
-    printf("> ");
-    char* str = fgets(buffer, kBufferSize, stdin);
-    if (str == NULL) break;
-    v8::HandleScope handle_scope;
-    ExecuteString(v8::String::New(str),
-                  v8::String::New("(shell)"),
-                  true,
-                  true);
-  }
-  printf("\n");
-}
-
-bool ExecuteString(v8::Handle<v8::String> source,
-                   v8::Handle<v8::Value> name,
-                   bool print_result,
-                   bool report_exceptions) {
-  v8::HandleScope handle_scope;
-  v8::TryCatch try_catch;
-  v8::Handle<v8::Script> script = v8::Script::Compile(source, name);
-  if (script.IsEmpty()) {
-    // Print errors that happened during compilation.
-    if (report_exceptions)
-      ReportException(&try_catch);
-    return false;
-  } else {
-    v8::Handle<v8::Value> result = script->Run();
-    if (result.IsEmpty()) {
-      // Print errors that happened during execution.
-      if (report_exceptions)
-        ReportException(&try_catch);
-      return false;
-    } else {
-      if (print_result && !result->IsUndefined()) {
-        // If all went well and the result wasn't undefined then print
-        // the returned value.
-        v8::String::Utf8Value str(result);
-        printf("%s\n", *str);
-      }
-      return true;
-    }
-  }
-}
-
 
 void ReportException(v8::TryCatch* try_catch) {
   v8::HandleScope handle_scope;
@@ -189,6 +95,61 @@ void ReportException(v8::TryCatch* try_catch) {
   fun->Call(context->ToObject(), 1, data);
 }
 
+bool ExecuteString(v8::Handle<v8::String> source, v8::Handle<v8::Value> name) {
+  v8::HandleScope handle_scope;
+  v8::TryCatch try_catch;
+  v8::Handle<v8::Script> script = v8::Script::Compile(source, name);
+  if (script.IsEmpty()) {
+    ReportException(&try_catch);
+    return false;
+  } else {
+    v8::Handle<v8::Value> result = script->Run();
+    if (result.IsEmpty()) {
+      ReportException(&try_catch);
+      return false;
+    }
+  }
+  return true;
+}
+
+void die(int code) {
+    int max = onexit->Length();
+    v8::Handle<v8::Function> fun;
+    for (int i=0;i<max;i++) {
+	fun = v8::Handle<v8::Function>::Cast(onexit->Get(v8::Integer::New(i)));
+	fun->Call(v8::Context::GetCurrent()->Global(), 0, NULL);
+    }
+    exit(code);
+}
+
+v8::Handle<v8::Value> _include(const v8::Arguments& args) {
+  for (int i = 0; i < args.Length(); i++) {
+    v8::HandleScope handle_scope;
+    v8::String::Utf8Value file(args[i]);
+    v8::Handle<v8::String> source = ReadFile(*file);
+    if (source.IsEmpty()) {
+      return v8::ThrowException(v8::String::New("Error loading file"));
+    }
+    
+    if (!ExecuteString(source, v8::String::New(*file))) {
+      return v8::ThrowException(v8::String::New("Error executing file"));
+    }
+  }
+  return v8::Undefined();
+}
+
+
+v8::Handle<v8::Value> _exit(const v8::Arguments& args) {
+  die(args[0]->Int32Value());
+  return v8::Undefined();
+}
+
+v8::Handle<v8::Value> _onexit(const v8::Arguments& args) {
+  onexit->Set(v8::Integer::New(onexit->Length()), args[0]);
+  return v8::Undefined();
+}
+
+
 int main(int argc, char ** argv, char ** envp) {
   v8::V8::SetFlagsFromCommandLine(&argc, argv, true);
   v8::HandleScope handle_scope;
@@ -207,35 +168,22 @@ int main(int argc, char ** argv, char ** envp) {
   SetupIo(context->Global());  
   
   #ifdef HAVE_MYSQL
-  SetupMysql(context->Global());  
+    SetupMysql(context->Global());  
   #endif
-
-  bool run_shell = (argc == 1);
-  for (int i = 1; i < argc; i++) {
-    const char* str = argv[i];
-    if (strcmp(str, "--shell") == 0) {
-      run_shell = true;
-    } else if (strcmp(str, "-f") == 0) {
-      // Ignore any -f flags for compatibility with the other stand-
-      // alone JavaScript engines.
-      continue;
-    } else if (strncmp(str, "--", 2) == 0) {
-      printf("Warning: unknown flag %s.\n", str);
-    } else {
-      // Use all other arguments as names of files to load and run.
-      v8::HandleScope handle_scope;
-      v8::Handle<v8::String> file_name = v8::String::New(str);
-      v8::Handle<v8::String> source = ReadFile(str);
-      if (source.IsEmpty()) {
-        printf("Error reading '%s'\n", str);
-        die(1);
-      }
-      if (!ExecuteString(source, file_name, false, true))
+  
+  if (argc == 1) {
+    printf("Nothing to do.\n");
+  } else {
+    char* str = argv[1];
+    v8::Handle<v8::String> name = v8::String::New(str);
+    v8::Handle<v8::String> source = ReadFile(str);
+    if (source.IsEmpty()) {
+	printf("Error reading '%s'\n", str);
         die(1);
     }
+    
+    int result = ExecuteString(source, name);
+    if (result) { die(result); }
   }
-  
-  
-  if (run_shell) RunShell(context);
   die(0);
 }

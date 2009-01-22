9411c80dd308fdf47117fb524143dd9ab736453e
ondrej.zara <none@none>
2009-01-22 09:12:41 +0000

    windows
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%40293

--- src/js_macros.h
+++ src/js_macros.h
@@ -7,7 +7,6 @@
 #  include <unistd.h>
 #  include <fcntl.h>
 #  include <string.h>
-#else
 #endif
 
 #define SAVE_PTR(index, ptr) args.This()->SetInternalField(index, v8::External::New((void *)ptr)); 
@@ -39,19 +38,39 @@ inline v8::Handle<v8::Array> JS_CHARARRAY(char * data, int count) {
 }
 
 inline void * my_read(char * name, size_t * size) {
+#ifdef HAVE_MMAP_H
 	int f = open(name, O_RDONLY);
 	if (f == -1) { return NULL; }
 	*size = lseek(f, 0, SEEK_END);
 	void * data = mmap(0, *size, PROT_READ, MAP_SHARED, f, 0);
 	close(f);
+#else
+	FILE * f = fopen(name, "rb");
+	if (f == NULL) { return NULL; }
+	fseek(f, 0, SEEK_END);
+	size_t s = ftell(f);
+	*size = s;
+	
+	rewind(f);
+	char * data = new char[s];
+	for (unsigned int i=0; i<s;) {
+		size_t read = fread(& data[i], 1, s-i, f);
+		i += read;
+	}
+#endif
 	return data;
 }
 
 inline void my_free(void * data, size_t size) {
+#ifdef HAVE_MMAP_H
 	munmap(data, size);
+#else
+	delete[] data;
+#endif
 }
 
 inline int my_write(char * name, void * data, size_t size) {
+#ifdef HAVE_MMAP_H
 	int f = open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (f == -1) { return -1; }
 	lseek(f, size - 1, SEEK_SET);
@@ -61,6 +80,12 @@ inline int my_write(char * name, void * data, size_t size) {
 	memcpy(dst, data, size);
 	munmap(dst, size);
 	close(f);
+#else
+	FILE * f = fopen(name, "wb");
+	if (f == NULL) { return -1; }
+	fwrite(data, size, 1,f);
+	fclose(f);
+#endif
 	return 0;
 }
 

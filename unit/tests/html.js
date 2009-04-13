a78dec5b44dba92e0849e656d812d2d9a00cd3a9
ondrej.zara <none@none>
2009-04-13 19:19:21 +0000

    more unit tests
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%40390

--- unit/tests/html.js
+++ unit/tests/html.js
@@ -4,6 +4,6 @@ var html = require("html").HTML;
 exports.testHtml = function() {
 	var s1 = "<a href='a&b'>\"";
 	var s2 = "&lt;a href=&apos;a&amp;b&apos;&gt;&quot;";
-	assert.assertEquals(html.escape(s1), s2, "html escape");
-	assert.assertEquals(html.unescape(s2), s1, "unescape");
+	assert.assertEquals(s2, html.escape(s1), "html escape");
+	assert.assertEquals(s1, html.unescape(s2), "unescape");
 }
--- unit/tests/util.js
+++ unit/tests/util.js
@@ -4,34 +4,34 @@ var util = require("util").Util;
 exports.testBase64 = function() {
 	var s1 = "hello";
 	var s2 = "aGVsbG8=";
-	assert.assertEquals(util.base64encode(s1), s2, "base64 encode");
-	assert.assertEquals(util.base64decode(s2), s1, "base64 decode");
+	assert.assertEquals(s2, util.base64encode(s1), "base64 encode");
+	assert.assertEquals(s1, util.base64decode(s2), "base64 decode");
 }
 
 exports.testSha1 = function() {
 	var s1 = "hello";
 	var s2 = "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d";
-	assert.assertEquals(util.sha1(s1), s2, "sha1");
+	assert.assertEquals(s2, util.sha1(s1), "sha1");
 }
 
 exports.testMd5 = function() {
 	var s1 = "hello";
 	var s2 = "5d41402abc4b2a76b9719d911017c592";
-	assert.assertEquals(util.md5(s1), s2, "md5");
+	assert.assertEquals(s2, util.md5(s1), "md5");
 }
 
 exports.testUtf8 = function() {
 	var s1 = "žšČ";
 	var s2 = String.fromCharCode(197, 190, 197, 161, 196, 140);
-	assert.assertEquals(util.utf8decode(s1), s2, "utf8 decode");
-	assert.assertEquals(util.utf8encode(s2), s1, "utf8 encode");
+	assert.assertEquals(s2, util.utf8decode(s1), "utf8 decode");
+	assert.assertEquals(s1, util.utf8encode(s2), "utf8 encode");
 }
 
 exports.testJson = function() {
 	var data = [1,"2",{"\n":[]},null,true];
 	var str = "[1,\"2\",{\"\\n\":[]},null,true]";
-	assert.assertEquals(util.serialize(data), str, "json serialize");
-	assert.assertEquals(util.serialize(util.deserialize(str)), str, "json deserialize");
+	assert.assertEquals(str, util.serialize(data), "json serialize");
+	assert.assertEquals(str, util.serialize(util.deserialize(str)), "json deserialize");
 	
 	var a = [];
 	a.push(a);

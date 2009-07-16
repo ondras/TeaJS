d34139e5f557d41265f9b18c8b9fb7309c21310b
jabis.is <none@none>
2009-07-16 18:46:32 +0000

    Added the initial process.exec stream buffer and JS_NULL to the macros
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%40529

--- src/js_macros.h
+++ src/js_macros.h
@@ -16,6 +16,44 @@
 #define JS_INT(val) v8::Integer::New(val)
 #define JS_FLOAT(val) v8::Number::New(val)
 #define JS_BOOL(val) v8::Boolean::New(val)
+/**
+ * Shorthands for various lengthy V8 syntaxt constructs.
+ */
+
+#ifndef _JS_MACROS_H
+#define _JS_MACROS_H
+
+#define _STRING(x) #x
+#define STRING(x) _STRING(x)
+
+#define SAVE_PTR(index, ptr) args.This()->SetInternalField(index, v8::External::New((void *)ptr)); 
+#define LOAD_PTR(index, type) reinterpret_cast<type>(v8::Handle<v8::External>::Cast(args.This()->GetInternalField(index))->Value());
+#define SAVE_VALUE(index, val) args.This()->SetInternalField(index, val)
+#define LOAD_VALUE(index) args.This()->GetInternalField(index)
+#define JS_STR(...) v8::String::New(__VA_ARGS__)
+#define JS_INT(val) v8::Integer::New(val)
+#define JS_FLOAT(val) v8::Number::New(val)
+#define JS_BOOL(val) v8::Boolean::New(val)
+#define JS_NULL v8::Null()
+#define JS_METHOD(name) v8::Handle<v8::Value> name(const v8::Arguments& args)
+#define JS_EXCEPTION(reason) v8::ThrowException(JS_STR(reason))
+#define JS_RETHROW(tc) v8::Local<v8::Value>::New(tc.Exception());
+
+#define JS_GLOBAL v8::Context::GetCurrent()->Global()
+#define GLOBAL_PROTO v8::Handle<v8::Object>::Cast(JS_GLOBAL->GetPrototype())
+#define APP_PTR reinterpret_cast<v8cgi_App *>(v8::Handle<v8::External>::Cast(GLOBAL_PROTO->GetInternalField(0))->Value());
+#define GC_PTR reinterpret_cast<GC *>(v8::Handle<v8::External>::Cast(GLOBAL_PROTO->GetInternalField(1))->Value());
+
+#define ASSERT_CONSTRUCTOR if (!args.IsConstructCall()) { return JS_EXCEPTION("Invalid call format. Please use the 'new' operator."); }
+
+#ifdef _WIN32
+#   define SHARED_INIT() extern "C" __declspec(dllexport) void init(v8::Handle<v8::Object> exports)
+#else
+#   define SHARED_INIT() extern "C" void init(v8::Handle<v8::Object> exports)
+#endif
+
+#endif
+
 #define JS_METHOD(name) v8::Handle<v8::Value> name(const v8::Arguments& args)
 #define JS_EXCEPTION(reason) v8::ThrowException(JS_STR(reason))
 #define JS_RETHROW(tc) v8::Local<v8::Value>::New(tc.Exception());
--- src/lib/process/js_process.cc
+++ src/lib/process/js_process.cc
@@ -3,6 +3,7 @@
  */
 
 #include <v8.h>
+#include <string>
 #include "js_macros.h"
 #include <stdlib.h>
 
@@ -10,13 +11,42 @@ namespace {
 
 JS_METHOD(_system) {
 	if (args.Length() != 1) {
-		return JS_EXCEPTION("Wrong argument count. Use system.system(\"command\")");
+		return JS_EXCEPTION("Wrong argument count. Use include(\"process\").system(\"command\")");
 	}
 	
 	v8::String::Utf8Value cmd(args[0]);
 	int result = system(*cmd);
 	return JS_INT(result);
 }
+
+/**
+ * The initial .exec method for stream buffering system commands
+ */
+
+JS_METHOD(_exec) {
+	if (args.Length() != 1) {
+		return JS_EXCEPTION("Wrong argument count. Use include(\"process\").exec(\"command\")");
+	}
+	
+	std::string data;
+	FILE *stream;
+	int MAX_BUFFER = 256;
+	char buffer[MAX_BUFFER];
+
+	v8::String::Utf8Value cmd(args[0]);
+	stream = popen(*cmd, "r");
+	while ( fgets(buffer, MAX_BUFFER, stream) != NULL ) {
+		data.append(buffer);
+	}
+	pclose(stream);
+	
+	if(data.c_str() != NULL) {
+	return JS_STR(data.c_str());
+	} else {
+	return JS_NULL;
+	}
+}
+
 
 }
 
@@ -25,4 +55,5 @@ SHARED_INIT() {
 	
 	/* this module provides a set of (static) functions */
 	exports->Set(JS_STR("system"), v8::FunctionTemplate::New(_system)->GetFunction());
+	exports->Set(JS_STR("exec"), v8::FunctionTemplate::New(_exec)->GetFunction());
 }

4f2b574ec563b678c90ae261118c35aff82dd598
ondrej.zara <none@none>
2011-12-03 20:06:40 +0000

    websocket example
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%40950

---
+++ example/websocket.js
@@ -0,0 +1,21 @@
+var Server = require("websocket").Server;
+var ws = new Server("0.0.0.0", 8888);
+
+var clients = [];
+var app = {
+	onmessage: function(client, data) {
+		for (var i=0;i<clients.length;i++) {
+			ws.send(clients[i], data);
+		}
+	},
+	onconnect: function(client, headers) {
+		clients.push(client);
+	},
+	ondisconnect: function(client, code, message) {
+		var index = clients.indexOf(client);
+		if (index != -1) { clients.splice(index, 1); }
+	}
+};
+
+ws.addApplication(app);
+ws.run();

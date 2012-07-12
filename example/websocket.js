d773177a445e603982973a47f33934504ae6e946
ondrej.zara <none@none>
2012-07-12 13:10:32 +0000

    websocket refactored to use eventloop module
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%401025

--- example/websocket.js
+++ example/websocket.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env v8cgi
 
-/* simple websocket echo server: repeats received message to all clients */
+/* simple websocket echo server: repeats received message to all clients (and logs to console) */
 
 var Server = require("websocket").Server;
-var ws = new Server("0.0.0.0", 8888);
+var ws = new Server("0.0.0.0", 8888, {idle:150});
 
 var clients = [];
 var app = {
 	onmessage: function(client, data) {
+		system.stdout.writeLine(data);
 		for (var i=0;i<clients.length;i++) {
 			ws.send(clients[i], data);
 		}
@@ -18,6 +19,10 @@ var app = {
 	ondisconnect: function(client, code, message) {
 		var index = clients.indexOf(client);
 		if (index != -1) { clients.splice(index, 1); }
+	},
+	onidle: function() {
+		system.stdout.write(".");
+		system.stdout.flush();
 	}
 };
 

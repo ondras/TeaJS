f3414a95cd66984029d0b70f4f9a5697d545b9d9
ondrej.zara <none@none>
2012-07-12 11:02:23 +0000

    event loop module
    --HG--
    extra : convert_revision : svn%3A45ebfb91-7180-4db0-a2bd-9f34e122093e/trunk%401024

---
+++ example/eventloop.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env v8cgi
+
+var eventloop = require("eventloop");
+var iterations = 0;
+
+var id = eventloop.setInterval(function() { 
+	iterations++;
+	system.stdout.writeLine("tick");
+	
+	if (iterations == 10) {
+		eventloop.setTimeout(function() {
+			system.stdout.writeLine("scheduled event");
+			eventloop.clearTimeout(id);
+		}, 1000);
+	}
+	
+}, 300);
+
+eventloop.run();
